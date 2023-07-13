15a4300bdba848cc897ad54fb98d59edb603687a
Yevhenii2022 <3starosta3@gmail.com>
2023-07-13 17:07:49 +0300

    finished hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,25 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const ctrl = require('../../controllers/contacts');
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { validateBody } = require('../../middlewares');
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const schemas = require('../../schemas/contacts');
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', ctrl.getAllContacts);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:id', ctrl.getContactsById);
 
-module.exports = router
+router.post('/', validateBody(schemas.addContactSchema), ctrl.addContact);
+
+router.delete('/:id', ctrl.deleteContactById);
+
+router.put(
+	'/:id',
+	validateBody(schemas.addContactSchema),
+	ctrl.updateContactById,
+);
+
+module.exports = router;
