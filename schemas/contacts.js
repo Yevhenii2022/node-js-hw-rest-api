e1d0772149fe27b7cd0499dd0821394775dd48ce
Yevhenii2022 <3starosta3@gmail.com>
2023-07-14 14:28:33 +0300

    add

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -6,6 +6,16 @@ const addContactSchema = Joi.object({
 	phone: Joi.string().required(),
 });
 
+const joiSchema = Joi.object({
+	a: Joi.string().min(2).max(10).required().messages({
+		'string.base': `"a" should be a type of 'text'`,
+		'string.empty': `"a" cannot be an empty field`,
+		'string.min': `"a" should have a minimum length of {#limit}`,
+		'any.required': `"a" is a required field`,
+	}),
+});
+
 module.exports = {
 	addContactSchema,
+	joiSchema,
 };
