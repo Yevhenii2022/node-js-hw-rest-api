a4c89d1b154cf4e6a8176caaffa54626e4c306b7
Yevhenii2022 <3starosta3@gmail.com>
2023-08-19 09:20:47 +0300

    update readme.md

--- test/login.test.js
+++ test/login.test.js
@@ -4,73 +4,73 @@
 3. It must be an object "user" with two fields inside the response: "email" and "subscription" with data-type String.
 */
 
-// const express = require('express');
-// const request = require('supertest');
-// const mongoose = require('mongoose');
-// const jwt = require('jsonwebtoken');
-// const { login } = require('../controllers/auth/index');
+const express = require('express');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const { login } = require('../controllers/auth/index');
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// const { DB_HOST, JWT_SECRET } = process.env;
+const { DB_HOST, JWT_SECRET } = process.env;
 
-// const app = express();
-// app.use(express.json());
-// app.post('/api/auth/login', login);
+const app = express();
+app.use(express.json());
+app.post('/api/auth/login', login);
 
-// describe('test loginUser controller', () => {
-// 	beforeAll(async () => {
-// 		await mongoose.connect(DB_HOST);
-// 	});
-// 	afterAll(async () => await mongoose.connection.close());
+describe('test loginUser controller', () => {
+	beforeAll(async () => {
+		await mongoose.connect(DB_HOST);
+	});
+	afterAll(async () => await mongoose.connection.close());
 
-// 	const sendingObject = { password: '123456', email: 'evgen@gmail.com' };
+	const sendingObject = { password: '123456', email: 'evgen@gmail.com' };
 
-// 	test('answer status-code equals 200', async () => {
-// 		const expectedStatusCode = 200;
+	test('answer status-code equals 200', async () => {
+		const expectedStatusCode = 200;
 
-// 		const response = await request(app)
-// 			.post('/api/auth/login')
-// 			.send(sendingObject);
+		const response = await request(app)
+			.post('/api/auth/login')
+			.send(sendingObject);
 
-// 		expect(response.statusCode).toBe(expectedStatusCode);
-// 	});
+		expect(response.statusCode).toBe(expectedStatusCode);
+	});
 
-// 	test('there is a token in the response', async () => {
-// 		const expectedFieldName = 'token';
-// 		const typeOfToken = 'string';
+	test('there is a token in the response', async () => {
+		const expectedFieldName = 'token';
+		const typeOfToken = 'string';
 
-// 		const response = await request(app)
-// 			.post('/api/auth/login')
-// 			.send(sendingObject);
+		const response = await request(app)
+			.post('/api/auth/login')
+			.send(sendingObject);
 
-// 		expect(Object.keys(response.body).includes(expectedFieldName)).toBeTruthy();
-// 		expect(typeof response.body[expectedFieldName]).toBe(typeOfToken);
-// 		expect(() => {
-// 			jwt.verify(response.body[expectedFieldName], JWT_SECRET);
-// 		}).not.toThrow(Error);
-// 	});
+		expect(Object.keys(response.body).includes(expectedFieldName)).toBeTruthy();
+		expect(typeof response.body[expectedFieldName]).toBe(typeOfToken);
+		expect(() => {
+			jwt.verify(response.body[expectedFieldName], JWT_SECRET);
+		}).not.toThrow(Error);
+	});
 
-// 	test("there is an object 'user' with defined fields in the response", async () => {
-// 		const expectedObjectName = 'user';
-// 		const expectedFieldsNames = ['email', 'subscription'];
-// 		const typeOfData = 'string';
+	test("there is an object 'user' with defined fields in the response", async () => {
+		const expectedObjectName = 'user';
+		const expectedFieldsNames = ['email', 'subscription'];
+		const typeOfData = 'string';
 
-// 		const response = await request(app)
-// 			.post('/api/auth/login')
-// 			.send(sendingObject);
+		const response = await request(app)
+			.post('/api/auth/login')
+			.send(sendingObject);
 
-// 		expect(
-// 			Object.keys(response.body).includes(expectedObjectName),
-// 		).toBeTruthy();
-// 		expect(Object.keys(response.body[expectedObjectName])).toEqual(
-// 			expectedFieldsNames,
-// 		);
-// 		expect(
-// 			typeof response.body[expectedObjectName][expectedFieldsNames[0]],
-// 		).toBe(typeOfData);
-// 		expect(
-// 			typeof response.body[expectedObjectName][expectedFieldsNames[1]],
-// 		).toBe(typeOfData);
-// 	});
-// });
+		expect(
+			Object.keys(response.body).includes(expectedObjectName),
+		).toBeTruthy();
+		expect(Object.keys(response.body[expectedObjectName])).toEqual(
+			expectedFieldsNames,
+		);
+		expect(
+			typeof response.body[expectedObjectName][expectedFieldsNames[0]],
+		).toBe(typeOfData);
+		expect(
+			typeof response.body[expectedObjectName][expectedFieldsNames[1]],
+		).toBe(typeOfData);
+	});
+});
